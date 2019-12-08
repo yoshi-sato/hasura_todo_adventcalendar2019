310e42d1fef332b73946a8f5e4d5fd6badfd9e52
里井嘉真 <>
2019-12-08 13:38:33 +0900

    とりあえず体裁

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,10 @@
+<template>
+    <li :key="todo.id">{{ todo.title }}</li>
+</template>
+
+<script>
+export default {
+    name: "TodoItem",
+    props: ["todo"]
+}
+</script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,26 @@
+<template>
+    <ul>
+        <TodoItem v-for="todo in todos" :key="todo.id" :todo="todo"/>
+    </ul>
+</template>
+
+<script>
+import TodoItem from './TodoItem.vue';
+
+export default {
+    name: 'TodoList',
+    data() {
+        return {
+            todos: [
+                {
+                    id:1,
+                    title:'test'
+                }
+            ]
+        } 
+    },
+    components: {
+        TodoItem
+    }
+}
+</script>
